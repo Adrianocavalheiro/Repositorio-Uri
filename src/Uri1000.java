83fbfa19b9272f5463cca23b9651fecd6ac5bae4
unknown <o_serial@hotmail.com>
2021-06-19 14:50:06 -0300

    Uri1000 resolvido

---
+++ src/Uri1000.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Uri1000 {
+
+    public static void main(String[] args) throws IOException {
+
+        // sem entrada
+        // saida Hello World!
+
+        System.out.println("Hello World!");
+
+    }
+
+}
