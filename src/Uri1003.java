78cc3ce5e511ae243145e2a73d642a3797efbb0a
unknown <o_serial@hotmail.com>
2021-06-19 15:46:17 -0300

    Uri1003 resolvido

---
+++ src/Uri1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Uri1003 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        int soma;
+
+        soma = X + Y;
+
+        System.out.println("SOMA = " + soma);
+
+        scanner.close();
+    }
+}
