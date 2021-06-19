25c7e2415138488a5a833fccb195da579a680fd2
unknown <o_serial@hotmail.com>
2021-06-19 16:31:41 -0300

    Uri1007 resolvido

---
+++ src/Uri1007.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1007 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        int DIFERENCA;
+
+        DIFERENCA = (A*B-C*D);
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+        scanner.close();
+    }
+}
